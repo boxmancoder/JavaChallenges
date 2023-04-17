88a7dad572686e6f672a80743d57831fd91e75e5
Isaac Francisco <isaacfrancisco305@gmail.com>
2023-04-17 11:42:00 -0400

    first commit

---
+++ Workbook1/JavaChallenges/src/main/java/org/yup/JavaChallengesApp.java
@@ -0,0 +1,8 @@
+package org.yup;
+
+public class JavaChallengesApp {
+
+        public static void main(String[] args){
+        System.out.println("Hello from the Java Challenges App");
+        }
+}
